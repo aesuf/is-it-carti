fd6ac403adc1abb09b18892defeb8d83e7cf85c2
quintenrivers <32085810+quintenrivers@users.noreply.github.com>
2018-10-25 17:52:26 -0400

    cleanup

--- QR/clean_mfcc.py
+++ QR/clean_mfcc.py
@@ -7,6 +7,7 @@ from pathlib import Path
 in_file  = None
 out_file = None
 argv = sys.argv[1:]
+out_df = []
 
 try:
 	opts,args = getopt.getopt(argv,"i:o:",["input=","output="])
@@ -25,39 +26,19 @@ try:
 except:
 	print('Invalid input')
 
-out_df  = []
-
 for col in in_df:
 	out_df.append(in_df[col].std())
 	out_df.append(in_df[col].mean())
 
 out_df = pd.DataFrame.from_dict(out_df).T
-columns = ['mffc_a_std',
-					 'mffc_a_mean',
-					 'mffc_b_std',
-					 'mffc_b_mean',
-					 'mffc_c_std',
-					 'mffc_c_mean',
-					 'mffc_d_std',
-					 'mffc_d_mean',
-					 'mffc_e_std',
-					 'mffc_e_mean',
-					 'mffc_f_std',
-					 'mffc_f_mean',
-					 'mffc_g_std',
-					 'mffc_g_mean',
-					 'mffc_h_std',
-					 'mffc_h_mean',
-					 'mffc_i_std',
-					 'mffc_i_mean',
-					 'mffc_j_std',
-					 'mffc_j_mean',
-					 'mffc_k_std',
-					 'mffc_k_mean',
-					 'mffc_l_std',
-					 'mffc_l_mean',
-					 'mffc_m_std',
-					 'mffc_m_mean']
+columns = ['mffc_a_std','mffc_a_mean','mffc_b_std','mffc_b_mean',
+					 'mffc_c_std','mffc_c_mean','mffc_d_std','mffc_d_mean',
+					 'mffc_e_std','mffc_e_mean','mffc_f_std','mffc_f_mean',
+					 'mffc_g_std','mffc_g_mean','mffc_h_std','mffc_h_mean',
+					 'mffc_i_std','mffc_i_mean','mffc_j_std','mffc_j_mean',
+					 'mffc_k_std','mffc_k_mean','mffc_l_std','mffc_l_mean',
+					 'mffc_m_std','mffc_m_mean']
+
 out_df.columns = columns
 
 if out_file.is_file():
